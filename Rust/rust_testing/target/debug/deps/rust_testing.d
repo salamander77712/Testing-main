ddd2a9e360f23b21b272f26ea3b5e4032641f651
Alexander Wright <xander.h.wright@gmail.com>
2022-08-09 19:50:06 -0400

    Starting to learn Rust

---
+++ Rust/rust_testing/target/debug/deps/rust_testing.d
@@ -0,0 +1,5 @@
+C:\Git\Testing-main\Rust\rust_testing\target\debug\deps\rust_testing.exe: src\main.rs
+
+C:\Git\Testing-main\Rust\rust_testing\target\debug\deps\rust_testing.d: src\main.rs
+
+src\main.rs:
---
+++ Rust/rust_testing/target/debug/rust_testing.d
@@ -0,0 +1,1 @@
+C:\Git\Testing-main\Rust\rust_testing\target\debug\rust_testing.exe: C:\Git\Testing-main\Rust\rust_testing\src\main.rs
