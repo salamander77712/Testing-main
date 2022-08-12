96135f7ccc1fc3368d1a8751575fad534ceb944a
Alexander Wright <xander.h.wright@gmail.com>
2022-08-11 23:12:48 -0400

    Made factorial funcion in Rust

--- Rust/rust_testing/src/main.rs
+++ Rust/rust_testing/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let corrects = [1,1,2,6,24,120,720,5040,40320,362880,3628800];
+    let mut i = 0;
+    for correct in corrects{
+        println!("{}", test(factorial(i), correct));
+        i += 1;
+    }
+}
+fn factorial(num: u32) -> u32{
+    if num == 0 || num == 1{
+        return 1;
+    }
+    let mut output = 1u32;
+    for n in 2..=num{
+        output *= n;
+    }
+    return output;
+}
+fn test(answer: u32, correct:u32) -> bool{
+    return answer == correct;
 }
